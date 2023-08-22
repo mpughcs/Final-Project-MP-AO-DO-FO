71ebc7cfecc1cd7114b0aa0b25e679e08335a246
Max Pugh <maxpugh@Maxs-MBP.lan>
2023-08-22 16:08:51 -0700

    test packages

--- gamestore/src/main/java/com/company/gamestore/repositories/ConsoleRepo.java
+++ gamestore/src/main/java/com/company/gamestore/repositories/ConsoleRepo.java
@@ -10,4 +10,5 @@ import java.util.List;
 public interface ConsoleRepo extends JpaRepository<Console,Integer> {
     List<Console> findByManufacturer(String manufacturer);
 
+
 }
---
+++ gamestore/src/test/java/com/company/gamestore/repositories/ConsoleRepoTest.java
@@ -0,0 +1,7 @@
+package com.company.gamestore.repositories;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ConsoleRepoTest {
+
+}
