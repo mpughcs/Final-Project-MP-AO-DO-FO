f49815d8e1033f9fc5c55379432dd2f3ece9da46
dustyjem <97073289+dustyjem@users.noreply.github.com>
2023-08-22 17:43:44 -0600

    Tax Model Update

--- gamestore/src/main/java/com/company/gamestore/models/Tax.java
+++ gamestore/src/main/java/com/company/gamestore/models/Tax.java
@@ -1,11 +1,12 @@
 package com.company.gamestore.models;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import javax.persistence.*;
 
 @Entity
+@JsonIgnoreProperties({"hibernateLazyInitializer","handler"})
+@Table(name = "tax")
 public class Tax {
 
     @Id
